cb0289770b3baac9004e33d809c077e29a58c74c
skjaved633 <skjaved633@gmail.com>
2021-08-22 18:06:47 +0530

    commit

--- selfP/a.js
+++ selfP/a.js
@@ -1,4 +1,4 @@
-async function A() {
+function A() {
     // var x = {},
     //     y = {
     //         key: "y"
@@ -12,18 +12,18 @@ async function A() {
     // x[y] = 11;
     // x[z] = 21;
     // x[a] = 43;
-    //consolw.log(x[y])
+    // console.log(x[y])
 
     //q2//
-    // var i = 0;
-    // do {
-    //     i++;
-    //     if (i == 5) {
-    //         console.log("work");
-    //         continue;
-    //         i = 0;
-    //     }
-    // } while (i < 7);
+    var i = 0;
+    do {
+        i++;
+        if (i == 5) {
+            console.log("work");
+            continue;
+            i = 0;
+        }
+    } while (i < 7);
 
     //Q3//
     var cars = 2;
